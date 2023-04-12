c2b71910e713ba16cc3ee1664f6bf723e28b1fa8
Francisco Berti da Cruz <fbc991@live.com>
2023-04-12 15:08:16 -0300

    ate aula 3. proxima 4

---
+++ src/containers/BarraLateral/index.tsx
@@ -0,0 +1,22 @@
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+
+const BarraLateral = () => {
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo type="text" placeholder="Procurar" />
+      </div>
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard ativo />
+      </S.Filtros>
+    </S.Aside>
+  )
+}
+
+export default BarraLateral
